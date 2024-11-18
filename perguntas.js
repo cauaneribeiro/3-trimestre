6c81191b9190e8d690e83e3c7cbfcc52f17bb894
cauaneribeiro <167082694+cauaneribeiro@users.noreply.github.com>
2024-11-18 08:51:28 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -15,9 +15,8 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
+    'Como se diz OI em inglês?',
     'Oi em ingles é HI (RAI)'
-)
+) 
